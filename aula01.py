56f1bc9838b3ebc5c9f666d8cd7acfc24ca87018
AndersSaints <anders.lsantos@gmail.com>
2023-10-08 01:58:43 +0100

    Update aula01

--- aula01.py
+++ aula01.py
@@ -1,2 +1,15 @@
-# Permite escrever um comentário
+"""
+DocString || Comentário de múltiplas linhas
+
+
+asdfg
+"""
+
+'''
+Usar para escrever suas notas
+'''
+
+# Permite escrever um comentário de uma linha
+print(123) # Na frente
+# Abaixo
 print(123)
